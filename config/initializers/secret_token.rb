f51a6e71e83834aeecde2cce2cb75dac17c42df8
irenemachine <irene.haque@gmail.com>
2012-05-10 19:45:05 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tweebune::Application.config.secret_token = '8d783cdb186927753d93b07cb28391057bc7fd255848aead8d96d7b605b9093e62e66254148882265fcb72540512c927777cbc51d835decded082e8d92ef9fcd'
