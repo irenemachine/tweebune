cf20f318cfa3f67faa8fd3e5714592f39852cd93
irenemachine <irene.haque@gmail.com>
2012-06-07 10:39:44 -0700

    catchall error message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  rescue_from Exception, :with => :render_error
+
+  def render_error exception
+    render "error/e500"
+    puts exception
+  end
 end
